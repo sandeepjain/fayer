694520a2a45b5204f9ab4a566f63218e97b4694f
Sandeep Jain <jainsandeep88@gmail.com>
2011-08-31 17:25:34 +0530

    Updated

---
+++ script.js
@@ -0,0 +1,35 @@
+(function (win, doc, undefined) {
+	var downloadMenu = doc.getElementById('downloadMenu'),
+		downloadSubmenu = doc.getElementById('downloadSubMenu');
+
+	if(downloadMenu.addEventListener) {
+		downloadMenu.addEventListener( "click", handleMenuClick);
+	} else if (downloadMenu.attachEvent) {
+		downloadMenu.attachEvent('click', handleMenuClick);
+	}
+	
+	function handleMenuClick(event) {
+		hideShowSubMenu(downloadSubmenu);
+		event.preventDefault();
+		return false;
+	}
+	
+	function hideShowSubMenu(ele) {
+		var status = getStyle(ele,'display');
+		if (status == '' || status == 'block') {
+			ele.style.display = 'none';
+		} else {
+			ele.style.display = 'block';
+		}
+	}
+	
+	function getStyle(ele,styleProp)
+	{
+		return (ele.currentStyle)
+			? x.currentStyle[styleProp]
+			: window.getComputedStyle
+				? document.defaultView.getComputedStyle(ele,null).getPropertyValue(styleProp)
+				: undefined;
+	}
+
+})(window, document);
